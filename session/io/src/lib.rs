92476e5443971b138cb6d4abe527d17f7c047ab4
typoraIO <typora.io@hotmail.com>
2025-04-02 22:03:59 +0800

    Update lib.rs

--- session/io/src/lib.rs
+++ session/io/src/lib.rs
@@ -1,30 +1,27 @@
 #![no_std]
 use gmeta::{In, InOut, Metadata};
-use gstd::{collections::HashMap, prelude::*, ActorId, MessageId};
+use gstd::{prelude::*, ActorId, MessageId};
 
-pub struct WordleMetadata;
+pub struct SessionMetadata;
 
-impl Metadata for WordleMetadata {
+impl Metadata for SessionMetadata {
     type Init = In<ActorId>;
-    type Handle = InOut<Action, SessionStatus>;
-    type Others = ();
-    type Reply = ();
+    type Handle = InOut<SessionAction, SessionEvent>;
+    type Reply = InOut<Event, ()>;
+    type Others = InOut<(), ()>;
     type Signal = ();
     type State = InOut<StateQuery, StateQueryReply>;
 }
-
-pub type State = HashMap<ActorId, Session>;
-
-type SentMessageId = MessageId;
-type OriginalMessageId = MessageId;
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Session {
-    pub msg_ids: (SentMessageId, OriginalMessageId),
+    pub target_program_id: ActorId,
     pub session_status: SessionStatus,
-    pub tries_number: u8,
+    pub start_block_height: u32,
+    pub attempts: u8,
+    pub msg_ids: (MessageId, MessageId),
 }
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
@@ -32,26 +29,10 @@ pub struct Session {
 #[scale_info(crate = gstd::scale_info)]
 pub enum SessionStatus {
     None,
-    GameStarted,
     Waiting,
     MessageSent,
+    MessageReceived(Event),
     GameOver(Outcome),
-    WordChecked {
-        user: ActorId,
-        correct_positions: Vec<u8>,
-        contained_in_word: Vec<u8>,
-    },
-    InvalidWord,
-    NoReplyReceived,
-}
-
-#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub enum Action {
-    StartGame,
-    CheckWord(String),
-    CheckGameStatus,
 }
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
@@ -60,6 +41,44 @@ pub enum Action {
 pub enum Outcome {
     Win,
     Lose,
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum SessionAction {
+    StartGame,
+    CheckWord(String),
+    CheckGameStatus,
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum SessionEvent {
+    GameStarted {
+        user: ActorId,
+    },
+    WordChecked {
+        user: ActorId,
+        correct_positions: Vec<u8>,
+        contained_in_word: Vec<u8>,
+    },
+    GameError(String),
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum Event {
+    GameStarted {
+        user: ActorId,
+    },
+    WordChecked {
+        user: ActorId,
+        correct_positions: Vec<u8>,
+        contained_in_word: Vec<u8>,
+    },
 }
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
